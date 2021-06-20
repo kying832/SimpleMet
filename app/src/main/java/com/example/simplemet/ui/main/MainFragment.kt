1c06e67172ce8463a2884c16b53d2ad37c8b988d
kying832 <kying832@gmail.com>
2021-06-20 09:09:09 -0700

    Added basic numberpicker
    TODO - harvest data from numberpicker to update viewmodel tempo.

--- app/src/main/java/com/example/simplemet/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/simplemet/ui/main/MainFragment.kt
@@ -6,27 +6,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.simplemet.R
+import com.example.simplemet.databinding.MainFragmentBinding
+import com.example.simplemet.databinding.MainFragmentBinding.inflate
+
 
 class MainFragment : Fragment() {
-
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        _binding = inflate(inflater, container, false)
+
+        val minValue = 40
+        val maxValue = 220
+
+        val initTempoPicker: Array<String> = Array((maxValue - minValue) / 4 + 1) { (it * 4 + 40).toString() }
+        binding.tempoPicker.minValue = 0
+        binding.tempoPicker.maxValue = initTempoPicker.size - 1
+        binding.tempoPicker.setDisplayedValues(initTempoPicker)
+
+        binding.tempoPicker.wrapSelectorWheel = true
+        //viewModel.tempo.value?.let { binding.tempoPicker.value = it }
+        
+        return binding.root
+        //return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
--- app/src/main/java/com/example/simplemet/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/simplemet/ui/main/MainViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.simplemet.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
+// TODO: Implement the ViewModel
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _tempo = MutableLiveData<Int>()
+    val tempo: LiveData<Int> = _tempo
+
+
+    init{
+        resetMet()
+    }
+
+    fun getFrequency(): Double {
+        return 1.0/ tempo.value!!.times(1.0 / 60.0)
+    }
+
+    fun resetMet(){
+        _tempo.value = 96
+    }
 }
